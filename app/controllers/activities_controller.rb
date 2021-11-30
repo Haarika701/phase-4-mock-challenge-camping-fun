48aa362f0353a7871554e9f8e87507056e378840
HaarikaR <hramadugu@gmail.com>
2021-11-30 10:37:46 -0800

    Done

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,21 +1,17 @@
 class ActivitiesController < ApplicationController
-    rescue_from ActiveRecord::RecordInvalid,with: :render_try_again
+    rescue_from ActiveRecord::RecordNotFound,with: :render_try_again
     def index
         activity = Activity.all 
         render json:activity
       end
 
       def destroy
-         activity = find_activity
+         activity =  Activity.find(params[:id])
          activity.destroy
          head :no_content
       end
 
       private
-
-      def find_activity
-        Activity.find(params[:id])
-      end
       
       def render_try_again
         render json: { error: "Activity not found" }, status: :not_found
--- app/controllers/campers_controller.rb
+++ app/controllers/campers_controller.rb
@@ -1,12 +1,12 @@
 class CampersController < ApplicationController
-    rescue_from ActiveRecord::RecordInvalid,with: :render_try_again
+    rescue_from ActiveRecord::RecordNotFound,with: :render_try_again
     rescue_from ActiveRecord::RecordInvalid,with: :render_unproccessed_entity
 def index
     render json: Camper.all
 end
 
 def show
-    camper = find_camper
+    camper =  Camper.find(params[:id])
     render json: camper, serializer: CamperActivitySerializer
   end
 
@@ -20,20 +20,17 @@ end
 private
 # t.string "name"
 # t.integer "age"
+
 def camper_params
     params.permit(:name,:age)
 end
-
-def find_camper
-    Camper.find(params[:id])
-  end
 
 def render_try_again
     render json: { error: "Camper not found" }, status: :not_found
 end
 
 def render_unproccessed_entity(exception)
-    render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+    render json: { errors: ["Validation errors"] }, status: :unprocessable_entity
 end
 
 end
